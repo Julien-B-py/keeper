5bb2ab50fa8b84ea41a14b725bf6422c29e9231e
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2021-12-28 16:53:58 +0100

    v0.1
    Create base app, components and styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return <footer >
+    <
+    p > ©{new Date().getFullYear()}, Julien BEAUJOIN < /p> < /
+  footer >
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return <div className = "notes" > < div className = "note" >
+    <
+    h1 > Title < /h1> <
+    p > Content < /p> < /
+  div > < /
+  div >
+}
+
+export default Note;
