612bdcd60822569689c1227e18ef2767cb6c6ca2
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-01-14 21:35:58 +0100

    v0.6.1
    Edit note animation on creation.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
-
 function Note(props) {
-
-   // store a reference to the note div
+  // store a reference to the note div
   const noteRef = useRef();
 
   const tlCreation = useRef();
@@ -16,39 +14,41 @@ function Note(props) {
   // only runs on first render
   // useLayoutEffect to avoiding flash of unstyled content
   useLayoutEffect(() => {
-        tlCreation.current = gsap.timeline()
-        .from(noteRef.current,{height:0, width:0, padding:0, margin:0})
-        .from(noteRef.current, { scale: 0 }, "-=0.15");
+    tlCreation.current = gsap
+      .timeline()
+      .from(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 })
+      .from(noteRef.current, { scale: 0 }, "-=0.15")
+      .from(noteRef.current.firstChild, { y: "-150%", autoAlpha: 0 })
+      .from(noteRef.current.children[1], { x: "-110%", autoAlpha: 0 }, "<")
+      .from(noteRef.current.children[2], { y: "220%", autoAlpha: 0 }, "<")
+      .from(noteRef.current.lastChild, { scale: 0 });
 
-        // cleanup function
-        return () => {
-          tlCreation.current.kill();
-        };
-
+    // cleanup function
+    return () => {
+      tlCreation.current.kill();
+    };
   }, []);
-
 
   // useEffect : wait until DOM has been rendered
   // Animate note on deletion
   // only runs when deleted value is changed to true
-    useEffect(() => {
+  useEffect(() => {
+    if (!deleted) return;
 
-      if (!deleted) return;
+    tlDeletion.current = gsap
+      .timeline({ onComplete: () => props.onDelete(props.id) })
+      .to(noteRef.current, {
+        scale: 0,
+        autoAlpha: 0,
+        duration: 1
+      })
+      .to(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 });
 
-    tlDeletion.current = gsap.timeline({onComplete: ()=> props.onDelete(props.id)})
-.to(noteRef.current, {
-    scale: 0,
-    autoAlpha: 0,
-    duration: 1
-  }).to(noteRef.current, {height:0, width:0, padding:0, margin:0}, "-=0.35")
-
-  // cleanup function
-  return () => {
-    tlDeletion.current.kill();
-  };
-
-        }, [deleted]);
-
+    // cleanup function
+    return () => {
+      tlDeletion.current.kill();
+    };
+  }, [deleted]);
 
   const deleteButton = (
     <button
