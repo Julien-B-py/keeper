93d36fd5b41baaca89d3b74683b9c574933faa2b
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-01-18 21:18:09 +0100

    v0.8
    Fix sample note mount and unmount animations.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,11 @@ import AddNoteForm from "./AddNoteForm";
 function App() {
   // State variable called "notes" to store all notes created by the user.
   const [notes, setNotes] = useState([]);
-
+  // State variable to deal with sample note conditional rendering
+  const [noNote, setNoNote] = useState(true);
 
   // Add a note to notes array
   function addNote(note) {
-
-
-
     // Update "notes" array
     setNotes(function (oldNotes) {
       // Using spread operator to return a new array containing oldNotes and the new note
@@ -26,17 +24,16 @@ function App() {
   function deleteNote(id) {
     // Update "notes" array
 
-
-
     setNotes(function (oldNotes) {
       // Using filter to return a new array where the requested note is removed based on his index.
       return oldNotes.filter((note) => note.id !== id);
     });
   }
 
-
-
-
+// Handle sample note display
+  function changeSampleNote(bool) {
+    setNoNote(bool);
+  }
 
   // Create a new Note components array from notes array.
   const listItems = notes.map((item) => (
@@ -48,36 +45,34 @@ function App() {
       date={item.date}
       onDelete={deleteNote}
       deleteButton={true}
+      notesCount={notes.length}
+      onHide={changeSampleNote}
     />
   ));
 
-  // If notes array is not empty render all the notes else render default example note
+  // If notes array is not empty render all the notes
+  // Display sample note only if noNote is set to True
   return (
     <div className="app">
       <Header />
       <div className="content">
         <AddNoteForm onAdd={addNote} />
 
-          {notes.length ? (
-        <div className="notes">
+        {notes && <div className="notes">{listItems.reverse()}</div>}
 
-            {listItems.reverse()}
-        </div>
-          ) : (
-                  <div className="notes">
+        {noNote && (
+          <div className="notes">
             <Note
               key={1}
               title="Example title"
               content="Example content"
               date="01/02/1993"
               deleteButton={false}
-
-
-
+              notesCount={notes.length}
+              onHide={changeSampleNote}
             />
-                    </div>
-          )}
-
+          </div>
+        )}
       </div>
       <Footer />
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -10,45 +10,66 @@ function Note(props) {
 
   const [deleted, setDeleted] = useState(false);
 
-  // Animate notes
+  // Animate user notes
   // Only runs when deleted value is changed
   // useLayoutEffect to avoiding flash of unstyled content
   useLayoutEffect(() => {
+    if (props.deleteButton) {
+      if (!deleted) {
+        tlCreation.current = gsap
+          .timeline()
+          .from(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 })
+          .from(noteRef.current, { scale: 0 }, "-=0.15")
+          .from(noteRef.current.firstChild, { y: "-150%", autoAlpha: 0 })
+          .from(noteRef.current.children[1], { x: "-110%", autoAlpha: 0 }, "<")
+          .from(noteRef.current.children[2], { y: "220%", autoAlpha: 0 }, "<")
+          .from(noteRef.current.children[3], { scale: 0 });
+      } else {
+        tlDeletion.current = gsap
+          .timeline({
+            onComplete: () => {
+              props.deleteButton && props.onDelete(props.id);
+              if (props.notesCount === 1) {
+                props.onHide(true);
+              }
+            }
+          })
+          .to(noteRef.current, {
+            scale: 0,
+            autoAlpha: 0
+          })
+          .to(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 });
+      }
+    }
+  }, [deleted]);
 
-if (!deleted) {
+  // Animate sample note disappearance
+  useEffect(() => {
+    if (!props.deleteButton) {
+      if (props.notesCount !== 0) {
+        tlDeletion.current = gsap
+          .timeline({ onComplete: () => props.onHide(false) })
+          .to(noteRef.current, {
+            scale: 0,
+            autoAlpha: 0
+          })
+          .to(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 });
+      }
+    }
+  }, [props.notesCount]);
 
-  tlCreation.current = gsap
-    .timeline()
-    .from(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 })
-    .from(noteRef.current, { scale: 0 }, "-=0.15")
-    .from(noteRef.current.firstChild, { y: "-150%", autoAlpha: 0 })
-    .from(noteRef.current.children[1], { x: "-110%", autoAlpha: 0 }, "<")
-    .from(noteRef.current.children[2], { y: "220%", autoAlpha: 0 }, "<")
-    .from(noteRef.current.children[3], { scale: 0 });
-
-} else {
-
-      tlDeletion.current = gsap
-        .timeline({ onComplete: () => props.deleteButton && props.onDelete(props.id) })
-        .to(noteRef.current, {
-          scale: 0,
-          autoAlpha: 0,
-        }).to(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 });
-
-
-  }}, [deleted]);
-
-
-
-
-
-
-
-
-
-
-
-
+  // Animate sample note appearance
+  useLayoutEffect(() => {
+    if (!props.deleteButton) {
+      tlCreation.current = gsap
+        .timeline()
+        .from(noteRef.current, { height: 0, width: 0, padding: 0, margin: 0 })
+        .from(noteRef.current, { scale: 0 }, "-=0.15")
+        .from(noteRef.current.firstChild, { y: "-150%", autoAlpha: 0 })
+        .from(noteRef.current.children[1], { x: "-110%", autoAlpha: 0 }, "<")
+        .from(noteRef.current.children[2], { y: "220%", autoAlpha: 0 }, "<");
+    }
+  }, [props.deleteButton]);
 
   const deleteButton = (
     <button
